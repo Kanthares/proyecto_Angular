0537551090a458651fc4f0359d3b4bf293695b18
Kanthares <tepargamer@gmail.com>
2022-09-15 01:58:29 -0400

    tarea

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule, Route} from '@angular/router';
+import { StaffComponent } from './staff/staff.component';
+import { CharactersComponent } from './characters/characters.component'; 
+import { StudentsComponent } from './students/students.component';
+import { DataService } from './data.service';
+
+
+const router: Route [] = [
+  {path: '', component: AppComponent},
+  {path: 'Staff', component: StaffComponent},
+  {path: 'Characters', component: CharactersComponent},
+  {path: 'Students', component: StaffComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StaffComponent,
+    CharactersComponent,
+    StudentsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule.forRoot(router)
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
